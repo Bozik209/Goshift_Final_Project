050d788d07f4bd6db6b2000d26c6578d2f89611e
YakirAvrahami <44398565+YakirAvrahami@users.noreply.github.com>
2019-08-05 13:26:18 +0300

    test

---
+++ app/src/main/java/com/example/boaz/big_project/yak.java
@@ -0,0 +1,14 @@
+package com.example.boaz.big_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class yak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.ma_who_can_work);
+    }
+}
